34c9e1dd069e477cc5726e18285aedc3a07f2b73
David Andersson <kallkorvhd@gmail.com>
2020-05-05 15:47:49 +0200

    Added temperature part

--- Hemsida/index.js
+++ Hemsida/index.js
@@ -1,97 +1,119 @@
- const Visitoruri = 'https://5ea6a04884f6290016ba6f0e.mockapi.io/api/v1/visitors'
+const uriVisitors = 'https://5ea6a04884f6290016ba6f0e.mockapi.io/api/v1/visitors'
 
-     loadData();
 
-   function loadData(){
+function loadData() {
 
-    const datadiv = document.getElementById('datadiv');
-    datadiv.innerHTML = '';
-    fetch(Visitoruri)
-        .then((resp) => resp.json())
-        .then(function (data) {
-            return data.map(function (data){
-            let divtag = document.createElement('div');
-                
-              divtag.innerHTML = `${data.name} <a href ="#" onclick="deleteVisitor(${data.id})" > Checka Ut</a>`;
-              datadiv.appendChild(divtag);
-        })
-        });
-   }
+  const datadiv = document.getElementById('datadiv');
+  datadiv.innerHTML = '';
+  fetch(uriVisitors)
+    .then((resp) => resp.json())
+    .then(function (data) {
+      return data.map(function (data) {
+        let divtag = document.createElement('div');
 
-   function createVisitor() {
+        divtag.innerHTML = `${data.name} <a href ="#" onclick="deleteVisitor(${data.id})" > Checka Ut</a>`;
+        datadiv.appendChild(divtag);
+      })
+    });
+}
 
-       
-    const textInput = document.getElementById('name').value;
-    var data = {name: textInput}
+function createVisitor() {
 
-    fetch(Visitoruri, 
+
+  const textInput = document.getElementById('name').value;
+  var data = { name: textInput }
+
+  fetch(uriVisitors,
     {
-        method: "POST",
-        mode: "cors",
-        cache: "no-cache",
-        credentials: "same-origin",
+      method: "POST",
+      mode: "cors",
+      cache: "no-cache",
+      credentials: "same-origin",
 
-        headers:
-        {
-            'Accept': 'application/json, text/plain, */*',
-            'Content-Type': 'application/json'
-        },
-        body: JSON.stringify(data)
+      headers:
+      {
+        'Accept': 'application/json, text/plain, */*',
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(data)
     })
     .then(res => res.json())
     .then(res => console.log(res))
 
-    setTimeout(() =>{
+  setTimeout(() => {
     loadData();
-    },500);
+  }, 500);
 
-    sendMail();
-   }
+  sendMail();
+}
 
-  function deleteVisitor(id) {
-      
-    fetch(Visitoruri + "/" + id, 
+function deleteVisitor(id) {
+
+  fetch(uriVisitors + "/" + id,
     {
-        method: "DELETE",
-        mode: "cors",
-        cache: "no-cache",
-        credentials: "same-origin",
+      method: "DELETE",
+      mode: "cors",
+      cache: "no-cache",
+      credentials: "same-origin",
 
-        headers:
-        {
-            'Accept': 'application/json, text/plain, */*',
-            'Content-Type': 'application/json'
-        },
+      headers:
+      {
+        'Accept': 'application/json, text/plain, */*',
+        'Content-Type': 'application/json'
+      },
     })
     .then(res => res.json())
     .then(res => console.log(res))
 
-    setTimeout(() =>{
+  setTimeout(() => {
     loadData();
-    },500);
-   }
-function trainTimes(){
+  }, 500);
+}
+function trainTimes() {
 
 
-   const departurediv = document.getElementById('departures');
-  
-      const Trainuri = 'https://cors-anywhere.herokuapp.com/http://api.sl.se/api2/realtimedeparturesV4.json?key=3027dcd2014b4a0d9e63e612f082d7e5&siteid=9297&timewindow=10';
-      fetch(Trainuri)
-        .then((resp) => resp.json())
-        .then(function (data) 
-        {
-            console.log(data)
-          let departures = data.ResponseData.Metros;
-          return departures.map(function (departure) {
-              let divtag = document.createElement('div');
-  
-              divtag.innerHTML = `${departure.LineNumber + " " + departure.Destination + " " + departure.DisplayTime}`;
-              departurediv.appendChild(divtag);
-          })
-        })
-        .catch(function (error) 
-        {
-          console.log(error);
-        });
+  const departurediv = document.getElementById('departures');
 
-    }
+  const uriDepartures = 'https://cors-anywhere.herokuapp.com/http://api.sl.se/api2/realtimedeparturesV4.json?key=3027dcd2014b4a0d9e63e612f082d7e5&siteid=9297&timewindow=10';
+  fetch(uriDepartures)
+    .then((resp) => resp.json())
+    .then(function (data) {
+      console.log(data)
+      let departures = data.ResponseData.Metros;
+      return departures.map(function (departure) {
+        let divtag = document.createElement('div');
+
+        divtag.innerHTML = `${departure.LineNumber + " " + departure.Destination + " " + departure.DisplayTime}`;
+        departurediv.appendChild(divtag);
+      })
+    })
+    .catch(function (error) {
+      console.log(error);
+    });
+
+}
+
+function getForecast() {
+
+  const divForecast = document.getElementById('divForecast');
+  const uriForecast = 'https://opendata-download-metfcst.smhi.se/api/category/pmp3g/version/2/geotype/point/lon/17.991/lat/59.2921/data.json';
+
+  fetch(uriForecast)
+    .then((resp) => resp.json())
+    .then(function (data) {
+      console.log(data)
+      let forecasts = data.timeSeries[0].parameters;
+
+      let divtag = document.createElement('div');
+
+      divtag.innerHTML = `${'Temperature: ' + forecasts[1].values[0]}`;
+      divForecast.appendChild(divtag);
+
+
+    })
+    .catch(function (error) {
+      console.log(error);
+    });
+
+}
+
