16c882769e8cb3a14b34bf30cf9301f65f94b823
David Andersson <kallkorvhd@gmail.com>
2020-05-11 11:22:33 +0200

    Finished website, added powerpoint

--- Hemsida/index.js
+++ Hemsida/index.js
@@ -1,28 +1,42 @@
 const uriVisitors = 'https://5ea6a04884f6290016ba6f0e.mockapi.io/api/v1/visitors'
 const uriEmployees = 'https://5ea6a04884f6290016ba6f0e.mockapi.io/api/v1/employees'
-var visitorCounter = 0;
+
+
 
 function loadDataVisitors() {
 
+    var visitorCounter = 0;
     const datadiv = document.getElementById('divVisitors');
     datadiv.innerHTML = '';
     fetch(uriVisitors)
         .then((resp) => resp.json())
         .then(function(data) {
-            return data.map(function(data) {
+            data.map(function(data) {
                 let divtag = document.createElement('div');
+                 visitorCounter++;
 
-                divtag.innerHTML = `${data.name} <a href ="#" onclick="deleteVisitor(${data.id})" > Checka Ut</a>` + " Visitors: " + visitorCounter;
+                 console.log(visitorCounter);
+
+                  
+               
+                    
+        
+
+                divtag.innerHTML = `${data.name} <a href ="#" onclick="deleteVisitor(${data.id})" > Checka Ut</a>`;
             
+                document.getElementById("divVisitorCounter").innerHTML = "Visitors in the building right now: " + visitorCounter;
+
             //divtag.innerHTML = `${data.name} - Status: ${data.status} - <a href ="#" onclick="changeStatus('På lunch', ${data.id})" >På lunch</a> - <a href ="#" onclick="changeStatus('I möte', ${data.id})" >I möte</a> - <a href ="#" onclick="changeStatus('Tillgänglig', ${data.id})" >Tillgänglig</a> - <a href ="#" onclick="deleteEmployee(${data.id})" >Checka ut</a>`;
 
                 datadiv.appendChild(divtag);
           })
+
         });
 }
 
 function loadDataEmployees() {
 
+    var employeeCounter = 0;
     const datadiv = document.getElementById('divEmployees');
     datadiv.innerHTML = '';
     fetch(uriEmployees)
@@ -30,22 +44,27 @@ function loadDataEmployees() {
         .then(function(data) {
             return data.map(function(data) {
                 let divtag = document.createElement('div');
+                employeeCounter++;
+
+                
 
                 divtag.innerHTML = `${data.name} <a href ="#" onclick="deleteEmployee(${data.id})" > Checka Ut</a>`;
 
-            //divtag.innerHTML = `${data.name} - Status: ${data.status} - <a href ="#" onclick="changeStatus('På lunch', ${data.id})" >På lunch</a> - <a href ="#" onclick="changeStatus('I möte', ${data.id})" >I möte</a> - <a href ="#" onclick="changeStatus('Tillgänglig', ${data.id})" >Tillgänglig</a> - <a href ="#" onclick="deleteEmployee(${data.id})" >Checka ut</a>`;
+                document.getElementById("divEmployeeCounter").innerHTML = "Employees in the building right now: " + employeeCounter;
+           
 
                 datadiv.appendChild(divtag);
             })
         });
+
+        
 }
 
 function createVisitor() {
 
 
-    const textInput = document.getElementById('name').value;
+    const textInput = document.getElementById('nameVisitor').value;
     var data = { name: textInput }
-    visitorCounter +=1;
 
     fetch(uriVisitors, {
             method: "POST",
@@ -71,9 +90,7 @@ function createVisitor() {
 
 function deleteVisitor(id) {
 
-    if(visitorCounter > 0) {
-        visitorCounter -=1;
-    }
+
     
     fetch(uriVisitors + "/" + id, {
             method: "DELETE",
@@ -97,7 +114,7 @@ function deleteVisitor(id) {
 function createEmployee() {
 
 
-    const textInput = document.getElementById('name').value;
+    const textInput = document.getElementById('nameEmployee').value;
     var data = { name: textInput }
 
     fetch(uriEmployees, {
@@ -142,24 +159,61 @@ function deleteEmployee(id) {
     }, 500);
 }
 
-function trainTimes() {
+function metroTimes() {
 
 
-    const divDepartures = document.getElementById('divDepartures');
+    const divMetroDepartures = document.getElementById('divMetroDepartures');
 
-    const uriDepartures = 'https://cors-anywhere.herokuapp.com/http://api.sl.se/api2/realtimedeparturesV4.json?key=3027dcd2014b4a0d9e63e612f082d7e5&siteid=9297&timewindow=10';
+    const uriDepartures = 'https://cors-anywhere.herokuapp.com/http://api.sl.se/api2/realtimedeparturesV4.json?key=26e72a20505e43508d8833d3c8ff0b49&siteid=9297&timewindow=10';
     fetch(uriDepartures)
         .then((resp) => resp.json())
         .then(function(data) {
             console.log(data)
+            
             let departures = data.ResponseData.Metros;
+            divMetroDepartures.innerHTML= "";
             return departures.map(function(departure) {
                 let divtag = document.createElement('div');
 
                 divtag.innerHTML = `${departure.LineNumber + " " + departure.Destination + " " + departure.DisplayTime}`;
-                divDepartures.appendChild(divtag);
+                divMetroDepartures.appendChild(divtag);
+
             })
         })
+
+        
+        .catch(function(error) {
+            console.log(error);
+        });
+
+}
+
+function busTimes() {
+
+
+    const divBusDepartures = document.getElementById('divBusDepartures');
+
+    const uriDepartures = 'https://cors-anywhere.herokuapp.com/http://api.sl.se/api2/realtimedeparturesV4.json?key=26e72a20505e43508d8833d3c8ff0b49&siteid=9297&timewindow=10';
+    fetch(uriDepartures)
+        .then((resp) => resp.json())
+        .then(function(data) {
+            console.log(data)
+            let departures = data.ResponseData.Buses;
+            divBusDepartures.innerHTML= "";
+            return departures.map(function(departure) {
+                let divtag = document.createElement('div');
+
+           divtag.innerHTML = `${departure.LineNumber + " " + departure.Destination + " " + departure.DisplayTime}`;
+                
+         
+            divBusDepartures.appendChild(divtag);
+               
+
+            })
+            
+        })
+
+        
         .catch(function(error) {
             console.log(error);
         });
@@ -170,6 +224,7 @@ function getForecast() {
 
     const divForecast = document.getElementById('divForecast');
     const uriForecast = 'https://opendata-download-metfcst.smhi.se/api/category/pmp3g/version/2/geotype/point/lon/17.991/lat/59.2921/data.json';
+var i;
 
     fetch(uriForecast)
         .then((resp) => resp.json())
@@ -178,10 +233,15 @@ function getForecast() {
             let forecasts = data.timeSeries[2].parameters;
 
             let divtag = document.createElement('div');
+            forecasts.forEach(element => {
+                if(element.name == "t")
+                divtag.innerHTML = `${'Temperature: ' + element.values[0] + '°C'}`;
+                divForecast.appendChild(divtag);
+    
+            });
 
-            divtag.innerHTML = `${'Temperature: ' + forecasts[11].values[0]}`;
-            divForecast.appendChild(divtag);
 
+          
 
         })
         .catch(function(error) {
@@ -203,14 +263,18 @@ function getTime() {
 
 
             times = new Date();
+            var currentTime = times.toLocaleTimeString();
+           var hours = times.getHours();
+            var minutes = times.getMinutes();
+           var seconds = times.getSeconds();
+        let divtag = document.createElement('div');
 
-            let divtag = document.createElement('div');
 
-
-            divtag.innerHTML = `${'Time: ' + times.toLocaleTimeString()}`;
-            divTime.appendChild(divtag);
-
-
+           //divtag.innerHTML = `${'Time: ' + times.toLocaleTimeString()}`;
+          
+           document.getElementById("time").innerHTML = currentTime;
+          // document.getElementById("time").divtag.innerHTML = times.toLocaleTimeString();
+           divTime.appendChild(divtag);
         })
         .catch(function(error) {
             console.log(error);
@@ -218,6 +282,7 @@ function getTime() {
 
 
 }
+
 
 function getJoke() {
 
@@ -241,3 +306,5 @@ function getJoke() {
             console.log(error);
         });
 }
+
+
